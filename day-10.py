5f52595ff558e79bd8ab395f4c9f3eabc7faa3f8
mgguisoben <mgguisoben@gmail.com>
2023-10-20 11:05:24 +0800

    Day 10 of 100 done

---
+++ day-10.py
@@ -0,0 +1,54 @@
+calculator = """
+ _____________________
+|  _________________  |
+| | JO           0. | |
+| |_________________| |
+|  ___ ___ ___   ___  |
+| | 7 | 8 | 9 | | + | |
+| |___|___|___| |___| |
+| | 4 | 5 | 6 | | - | |
+| |___|___|___| |___| |
+| | 1 | 2 | 3 | | x | |
+| |___|___|___| |___| |
+| | . | 0 | = | | / | |
+| |___|___|___| |___| |
+|_____________________|
+"""
+
+add = lambda x,y: x + y
+sub = lambda x,y: x - y
+mul = lambda x,y: x * y
+div = lambda x,y: x / y
+
+current_total = 0
+
+print(calculator)
+print("A simple calculator.")
+print("+\n-\n*\n/")
+
+num1 = float(input("What's your first number? "))
+
+calculating = True
+while calculating:
+
+    oper = input("Pick an operation: ")
+    num2 = float(input("What's the next number? "))
+
+    if oper == '+':
+        current_total = add(num1, num2)
+    elif oper == '-':
+        current_total = sub(num1, num2)
+    elif oper == '*':
+        current_total = mul(num1, num2)
+    elif oper == '/':
+        current_total = div(num1, num2)
+
+    print(f"{num1} {oper} {num2} = {current_total}")
+
+    calc_current = input(f"Type 'y' to continue calculating with {current_total}, "
+                          f"or type 'n' to start a new calculation: " )
+
+    if calc_current == 'y':
+        num1 = current_total
+    elif calc_current == 'n':
+        num1 = float(input("What's your first number? "))
