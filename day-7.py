dd286a0b4fcdfa1759f776516c548a545639c241
mgguisoben <mgguisoben@gmail.com>
2023-10-20 10:18:26 +0800

    Added clear terminal on every guess

--- day-7.py
+++ day-7.py
@@ -1,18 +1,21 @@
 from resources.day7.hangman_art import logo, stages
 from resources.day7.hangman_words import word_list
 import random
+import os
+import time
 
 word = list(random.choice(word_list))
 guesses = len(stages)
 correct_guesses = ['_' for _ in range(len(word))]
 already_guessed = []
 
-print(logo)
-print(f"The word has {len(word)} letters.")
-
 guessed = False
 
 while not guessed:
+
+    print(logo)
+    print(f"The word has {len(word)} letters.")
+    print(stages[6])
 
     if word == correct_guesses:
         print("You guessed it!")
@@ -26,22 +29,26 @@ while not guessed:
 
         if guess in already_guessed:
             print("You've already guessed that letter")
+            time.sleep(2)
+            os.system('cls')
 
         elif guess in word:
             already_guessed.append(guess)
             for index, letter in enumerate(word):
                 if guess == letter:
                     correct_guesses[index] = guess
+            os.system('cls')
 
         else:
             already_guessed.append(guess)
 
             if guesses == 1:
-                print(stages[0])
-                print("You just died.")
                 guessed = True
 
             else:
                 print(stages[guesses - 1])
-
             guesses -= 1
+        os.system('cls')
+
+print(stages[6])
+print("You just died.")
